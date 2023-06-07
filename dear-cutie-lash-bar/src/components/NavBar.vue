e2e0528a08d6f71fb15b930e1df8bef684ddeffa
Laurence <94482980+ld17282@users.noreply.github.com>
2023-06-06 21:29:53 -0700

    Improve styling of NavBar.vue component
    This commit refines the styling of the NavBar.vue component to enhance its visual appearance and improve user experience. The changes include:
    
    - Adjusting the alignment and spacing of the navigation elements for better visual balance.
    - Applying appropriate font sizes, colors, and typography styles to enhance readability.
    - Adding hover effects and transitions to interactive elements for better user feedback.
    - Ensuring consistent and responsive layout across different screen sizes.
    
    These style enhancements improve the overall aesthetic and usability of the NavBar.vue component, providing a more visually appealing and user-friendly navigation experience.

--- dear-cutie-lash-bar/src/components/NavBar.vue
+++ dear-cutie-lash-bar/src/components/NavBar.vue
@@ -9,15 +9,19 @@ import '../../style.css'
 </script>
 
 <template>
-    <div class="flex flex-row justify-center">
+    <div class="flex flex-col justify-center ">
       <img src="../assets/Site Logo.svg" class="logo" alt="DC logo" />
+      <hr class="h-px w-11/12 border-[#F880DD] border-1 self-center">
     </div>
   </template>
 
 <style>
   .logo {
-    height: 6em;
-    padding: 1.5em;
+    height: 3em;
+    width: auto;
+    margin-top: 1em;
+    margin-bottom: 1em;
+    padding-top: 0em;
     will-change: filter;
     transition: filter 300ms;
   }
